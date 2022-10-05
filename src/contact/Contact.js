0b35d5bdf2717a9aeae0cffaaeb37b32ccd003c7
Sam <samuel.burns.25@gmail.com>
2022-10-05 00:09:48 -0400

    quick updates to event & contact page

---
+++ src/contact/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '../App.css';
+
+const Contact = () => {
+  return (
+    <div className='page-body'>
+      <div className='container' >
+        <div className='contact'>
+        ✉ g.atallah@amoretalla.com<br></br>
+        ☏ (603) 685-8478<br></br><br></br>
+        
+        8 Stiles Road<br></br>
+        Salem, NH 03079
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
