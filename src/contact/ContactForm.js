94bb9b9819553adf1f9a809fd180930ef29c7b9b
Sam <samuel.burns.25@gmail.com>
2022-10-16 09:17:51 -0400

    adress and contacts added

--- src/contact/ContactForm.js
+++ src/contact/ContactForm.js
@@ -24,45 +24,53 @@ const ContactForm = () => {
   return (
     <div className='pageBody'>
       <div className='container containerContact'>
-        <form ref={form} onSubmit={sendEmail}>
-          <div className='form-group'>
-            <label htmlFor='firstName' className='form-text'>First Name</label>
-            <input type='text' name='firstName' className='form-control' />
+        <div className='row'>
+          <div className='col-md-8 col-sm-12'>
+            <form ref={form} onSubmit={sendEmail}>
+              <div className='form-group'>
+                <label htmlFor='firstName' className='form-text'>First Name</label>
+                <input type='text' name='firstName' className='form-control' />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='lastName' className='form-text'>Last Name</label>
+                <input type='text' name='lastName' className='form-control' />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='phoneNumber' className='form-text'>Phone Number</label>
+                <PhoneInput
+                  placeholder='Enter phone number'
+                  defaultCountry='US'
+                  value={value}
+                  onChange={setValue}
+                  type='text'
+                  name='phoneNumber'
+                  className='form-control'
+                />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='customerEmail' className='form-text'>Email address</label>
+                <input type='email' name='email' className='form-control' aria-describedby='emailHelp' />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='weddingDate' className='form-text'>Wedding Date (if known)</label>
+                <input type='date' name='weddingDate' className='form-control wedding-date' aria-describedby='date' />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='message' className='form-text'>Message</label>
+                <textarea className='form-control' name='contactMessage' rows='4'></textarea>
+              </div>
+              <button type='submit' value='Send' className='btn btnSubmit'>SUBMIT</button>
+            </form>
           </div>
-          <div className='form-group'>
-            <label htmlFor='lastName' className='form-text'>Last Name</label>
-            <input type='text' name='lastName' className='form-control' />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='phoneNumber' className='form-text'>Phone Number</label>
-            <PhoneInput
-              placeholder='Enter phone number'
-              defaultCountry='US'
-              value={value}
-              onChange={setValue}
-              type='text'
-              name='phoneNumber'
-              className='form-control'
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='customerEmail' className='form-text'>Email address</label>
-            <input type='email' name='email' className='form-control' aria-describedby='emailHelp' />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='weddingDate' className='form-text'>Wedding Date (if known)</label>
-            <input type='date' name='weddingDate' className='form-control wedding-date' aria-describedby='date' />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='message' className='form-text'>Message</label>
-            <textarea className='form-control' name='contactMessage' rows='4'></textarea>
-          </div>
-          <button type='submit' value='Send' className='btn btnSubmit'>SUBMIT</button>
-        </form>
-      </div>
+          <div className='col-md-4 col-sm-12 contactInfo'>
+            <div><a className='emailLink' href='mailto:g.atallah@amoretalla.com'>✉ g.atallah@amoretalla.com</a></div>
+            <div><a className='phoneLink' href='tel:603-685-8478'>☏ 603-685-8478</a></div>
 
-      <a className='emailLink' href='mailto:g.atallah@amoretalla.com'>✉</a>
-      <a className='phoneLink' href='tel:603-685-8478'>☏</a>
+            <div className='addressBlock'><a className='address' href='http://maps.google.com/?q=1200 Pennsylvania Ave SE, Washington, District of Columbia, 20003'>8 Stiles Road<br></br>
+            Salem, NH 03079</a></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
