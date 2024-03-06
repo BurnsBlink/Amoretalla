fc8886e298b280567624a59273dc389b0ee86a8d
Sam <samuel.burns.25@gmail.com>
2024-03-06 16:20:18 -0500

    main updates for chicago show

---
+++ src/util/utilities.js
@@ -0,0 +1,9 @@
+export const longForm = {
+  wedCompanyWeddingWear: {
+    quote: `I must compliment Amorétalla on the gowns we received. They are obviously stunning, but as a boutique manager, I cannot get over how well they are designed. Even the hang straps are well thought out. And don't get me started on how nicely the convertible skirts and trains go on and come off! It's an absolute privilege to work with these garments!!!! I'm having SO MUCH FUN!!!! Thank you!!!!`,
+    author: 'Erin Fairfield, Boutique Manager at Wed Company Wedding Wear'
+  },
+  mainPage: {
+    description: `At Amorétalla, every piece of the Bridal collection, from cutting-edge couture gowns to figure-flattering jumpsuits and rompers, is hand stitched in an atelier in Salem, N.H., using the finest fabrics, laces, crystals and other finishes mindfully curated from around the globe.`
+  }
+};
